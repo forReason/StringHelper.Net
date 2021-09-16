59cc53e0c8d22c627f5584c343c488366f638ad5
julian b <julian.bechtold@outlook.com>
2021-09-16 10:59:40 +0200

    Add project files.

---
+++ String_Helper_Project/Functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Helper_Project
+{
+    public class Functions
+    {
+        /// <summary>
+        /// takes a reference in memory to a string and calculates its start and end index for use in for loops (quick .Trim());
+        /// </summary>
+        /// <param name="input">a string which should be trimmed. Max length = int.Max value or string max length. Whatever is smaller</param>
+        /// <returns></returns>
+        public (int StartIndex, int EndIndex) TrimString(ref string input)
+        {
+            (int StartIndex, int EndIndex) trimIndex = (0, input.Length - 1);
+            // calculate end index (trim end)
+            for (int i = trimIndex.EndIndex; i >= 0; i--)
+            {
+                if (input[i] != ' ' && input[i] != '\t' && input[i] != '\n' && input[i] != '\r')
+                {
+                    trimIndex.EndIndex = i;
+                    break;
+                }
+            }
+            // it is an empty string!
+            if (trimIndex.EndIndex == 0) return (0, 0);
+            // calculate start index
+            for (int i = 0; i <= trimIndex.EndIndex; i++)
+            {
+                if (input[i] != ' ' && input[i] != '\t' && input[i] != '\n' && input[i] != '\r')
+                {
+                    trimIndex.StartIndex = i;
+                    return trimIndex;
+                }
+            }
+            // this point should never be reached
+            return (0, 0);
+        }
+        public int GetAmountOfLines(ref string input)
+        {
+            int lines = 1;
+            char lastChar = 'a';
+            foreach (char c in input)
+            {
+                if (c == '\r' || (lastChar != '\r' && c == '\n')) lines++;
+                lastChar = c;
+            }
+            return lines;
+        }
+    }
+}
---
+++ String_Helper_Project/InvariantString.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Helper_Project
+{
+    public class InvariantString
+    {
+        public string InvaryString(string input, bool toLower = false, bool trim = true)
+        {
+            if (trim) input = input.Trim();
+            if (toLower) input = input.ToLower().Trim();
+            StringBuilder output = new StringBuilder();
+            foreach (char c in input)
+            {
+                switch (c)
+                {
+                    case 'ä':
+                        output.Append("ae");
+                        break;
+                    case 'ö':
+                        output.Append("oe");
+                        break;
+                    case 'ü':
+                        output.Append("ue");
+                        break;
+                    case 'è':
+                        output.Append("e");
+                        break;
+                    case 'ê':
+                        output.Append("e");
+                        break;
+                    case 'é':
+                        output.Append("e");
+                        break;
+                    case 'ì':
+                        output.Append("i");
+                        break;
+                    case 'î':
+                        output.Append("i");
+                        break;
+                    case 'í':
+                        output.Append("i");
+                        break;
+                    case 'â':
+                        output.Append("a");
+                        break;
+                    case 'à':
+                        output.Append("a");
+                        break;
+                    case 'á':
+                        output.Append("a");
+                        break;
+                    default:
+                        output.Append(c);
+                        break;
+                }
+            }
+            return output.ToString();
+        }
+        public string CleanCharacters(string input, char[] charsToClean)
+        {
+            StringBuilder output = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (!charsToClean.Contains(c)) output.Append(c);
+            }
+            return output.ToString();
+        }
+    }
+}
