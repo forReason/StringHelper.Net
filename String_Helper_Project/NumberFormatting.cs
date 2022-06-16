df89c642e059c912aee35fcc7599e7bb091ecbaa
julian b <bechti44@googlemail.com>
2022-06-16 13:04:18 +0200

    added code to format floating point numbers without scientific notation. EG, 0.00005214541 would become 1E-5

---
+++ String_Helper_Project/NumberFormatting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Helper_Project
+{
+    public class NumberFormatting
+    {
+        public static string ToReadableString(double input)
+        {
+            return input.ToString(DoubleFixedPoint);
+        }
+        public const string DoubleFixedPoint = "0.###################################################################################################################################################################################################################################################################################################################################################";
+    }
+}
