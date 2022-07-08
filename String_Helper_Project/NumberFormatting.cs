577ecd8292ae49f9ed1299061ec886c80a3ad5bf
Julian B <bechti44@googlemail.com>
2022-07-08 17:37:35 +0200

    added functions for number formatting

--- String_Helper_Project/NumberFormatting.cs
+++ String_Helper_Project/NumberFormatting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace String_Helper_Project
@@ -13,5 +14,18 @@ namespace String_Helper_Project
             return input.ToString(DoubleFixedPoint);
         }
         public const string DoubleFixedPoint = "0.###################################################################################################################################################################################################################################################################################################################################################";
+        private static Regex Regex = new Regex("^(0.0*\\d{3})");
+        public static string TruncateSmallerZeroCryptoAmt(double amount)
+        {
+            if (amount >= 1)
+            { // in a range to show 2 decimals
+                return amount.ToString("0.##"); // eg 5.93
+            }
+            else {
+                var match = Regex.Match(ToReadableString(amount));
+                return match.Value.TrimEnd('0');
+            }
+            
+        }
     }
 }
