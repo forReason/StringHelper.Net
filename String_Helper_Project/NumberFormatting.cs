5ed94b8b71d1db5d2932bfd54f15eda7646317ed
Julian B <bechti44@googlemail.com>
2023-01-25 23:33:38 +0100

    added version number extraction function

--- String_Helper_Project/NumberFormatting.cs
+++ String_Helper_Project/NumberFormatting.cs
@@ -9,12 +9,37 @@ namespace String_Helper_Project
 {
     public class NumberFormatting
     {
+        /// <summary>
+        /// Extracts the version number from a given string.
+        /// </summary>
+        /// <param name="versionString">A string that may contain a version number.</param>
+        /// <returns>The version number as a string if found, or null if not found.</returns>
+        string ExtractVersionNumber(string versionString)
+        {
+            var versionRegex = new Regex(@"(\d+(\.\d+)*)");
+            var match = versionRegex.Match(versionString);
+            if (match.Success)
+            {
+                return match.Value;
+            }
+            return null;
+        }
+        /// <summary>
+        /// Converts a double to a string with a fixed number of decimal places.
+        /// </summary>
+        /// <param name="input">The double number to be converted</param>
+        /// <returns>The double number as a string with a fixed number of decimal places.</returns>
         public static string ToReadableString(double input)
         {
             return input.ToString(DoubleFixedPoint);
         }
         public const string DoubleFixedPoint = "0.###################################################################################################################################################################################################################################################################################################################################################";
         private static Regex Regex = new Regex("^(0.0*\\d{3})");
+        /// <summary>
+        /// Truncates a small decimal number to a certain number of decimal places. 
+        /// </summary>
+        /// <param name="amount">The decimal number to be truncated.</param>
+        /// <returns>The truncated decimal number as a string.</returns>
         public static string TruncateSmallerZeroCryptoAmt(double amount)
         {
             if (amount >= 1)
