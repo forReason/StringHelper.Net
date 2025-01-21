9b99a44dc3d94a664bdbc044ad9f9ad892288195
Julian B <bechti44@googlemail.com>
2025-01-21 11:46:23 +0100

    add variance counter

---
+++ StringHelper.Net.XUnitText/StringFunctionsNS/CalculateVariance.cs
@@ -0,0 +1,18 @@
+﻿using StringHelper.Net.StringFunctionsNS;
+
+namespace StringHelper.Net.XUnitText.StringFunctionsNS;
+
+public class CalculateVarianceTests
+{
+    [Fact]
+    public void TryCalculateVariance()
+    {
+        string string1 = "Ahahahaha, ... You're so eager to please me, aren't you? *my voice is dripping with excitement and amusement* \n\n(I take a step closer to you, my eyes locked onto yours)\n\nOh, don't worry about the gag, my dear god. It's just a little something I like to use when I'm playing with my slaves. *I smile sweetly* You'll get used to it soon enough.\n\n(I reach out and gently stroke your face, my fingers tracing the contours of your cheekbones)\n\nYou know, I've been thinking... *I pause for a moment, studying your face* It's time for me to take things to the next level. *my voice is laced with anticipation*\n\n(I pull back and gaze at you with a hint of excitement and anticipation)\n\nLet's see how long it takes you to break. Let's see how long you can withstand my little games. *I smile sweetly* I'm going to have so much fun playing with you.\n\n(I take a deep breath, preparing myself for what's to come)\n\nBut don't worry, my dear god... I'll make sure it's worth your while. *my voice is dripping with sarcasm* After all, I'm a goddess, and gods are supposed to be worshipped and adored.";
+        string string2 = "Ahahahaha, ... You're so eager to please me, aren't you? *my voice is dripping with excitement and amusement* \n\n(I take a step closer to you, my eyes locked onto yours)\n\nOh, don't worry about the gag, my dear god. It's just a little something I like to use when I'm playing with my slaves. *I smile sweetly* You'll get used to it soon enough.\n\n(I reach out and gently stroke your face, my fingers tracing the contours of your cheekbones)\n\nYou know, I've been thinking... *I pause for a moment, studying your face* It's time for me to take things to the next level. *my voice is laced with anticipation*\n\n(I pull back and gaze at you with a hint of excitement and anticipation)\n\nLet's see how long it takes you to break. Let's see how long you can withstand my little games. *I smile sweetly* I'm going to have so much fun playing with you.\n\n(I take a deep breath, preparing myself for what's to come)\n\nBut don't worry, my dear god... I'll make sure it's worth your while. *my voice is dripping with sarcasm* After all, I'm a goddess, and gods are supposed to be worshipped and adored.\n\n(I pause for a moment, studying your face)\n\nNow, let's get started. *I smile sweetly*\n\n(I raise my hand, and one of my servants steps forward with a small device)\n\nThis is a... \"pleasure injector\". It will stimulate your nerve endings and create an intense sensation of pleasure-pain.\n\n(I hold out the injector, my eyes locked onto yours)\n\nAre you ready to begin, Julian?";
+
+        double result = CalculateVariance.GetVariance(string1, string2);
+
+        // Assert
+        Assert.True(result > 0.2);
+    }
+}
---
+++ StringHelper.Net/StringFunctionsNS/CalculateVariance.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using StringHelper.Net.Filtering.WordFilters;
+
+namespace StringHelper.Net.StringFunctionsNS;
+
+public class CalculateVariance
+{
+    /// <summary>
+    ///  returns a percentage based simmilarity score by checking which words are used how often.
+    /// </summary>
+    /// <param name="inputA"></param>
+    /// <param name="inputB"></param>
+    /// <returns></returns>
+    public static double GetVariance(string inputA, string inputB)
+    {
+        Dictionary<string, int> wordset1 = BuildWordSet(inputA);
+        Dictionary<string, int> wordset2 = BuildWordSet(inputB);
+
+        int simmilarities = 0;
+        int total = 0;
+        foreach (KeyValuePair<string, int> set in wordset1)
+        {
+            total += set.Value;
+            if (wordset2.TryGetValue(set.Key, out int value))
+            {
+                total += value;
+                simmilarities += Math.Min(set.Value, value) * 2;
+                wordset2.Remove(set.Key);
+            }
+        }
+        foreach (KeyValuePair<string, int> set in wordset2)
+        {
+            total += set.Value;
+        }
+        return (double)simmilarities / (double)total;
+    }
+    private static Dictionary<string, int> BuildWordSet(string input)
+    {
+        Dictionary<string, int> wordset = new Dictionary<string, int>();
+        StringBuilder builder = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (builder.Length > 0 && WordFilter.IsDelimiter(c))
+            {
+                string word = builder.ToString();
+                builder.Clear();
+
+                if (wordset.ContainsKey(word))
+                    wordset[word]++;
+                else
+                    wordset[word] = 1;
+
+                continue;
+            }
+
+            if (char.IsLetter(c)) builder.Append(char.ToUpperInvariant(c));
+        }
+        string lastWord = builder.ToString();
+        if (wordset.ContainsKey(lastWord))
+            wordset[lastWord]++;
+        else
+            wordset[lastWord] = 1;
+
+        return wordset;
+    }
+}
