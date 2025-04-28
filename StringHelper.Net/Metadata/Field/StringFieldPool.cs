4dee8dd88637ff91f3cb3d31830e8bf58097a6df
Julian B <bechti44@googlemail.com>
2025-04-28 14:15:34 +0200

    add reference counting for stringfields

--- StringHelper.Net/Metadata/Field/StringFieldPool.cs
+++ StringHelper.Net/Metadata/Field/StringFieldPool.cs
@@ -6,37 +6,56 @@ namespace StringHelper.Net.Metadata.StringField;
 
 public class StringFieldPool
 {
-    public NamedTokenPool<FieldName> FieldNames = new ((id, name) => new FieldName(id, name));
-    public NamedTokenPool<FieldValue> FieldValues = new ((id, name) => new FieldValue(id, name));
+    public NamedTokenPool<FieldName> FieldNames = new((id, name) => new FieldName(id, name));
+    public NamedTokenPool<FieldValue> FieldValues = new((id, name) => new FieldValue(id, name));
 
     public ConcurrentDictionary<(FieldName, FieldValue), StringField> AllFields
         = new ConcurrentDictionary<(FieldName, FieldValue), StringField>();
+
+    private readonly ConcurrentDictionary<FieldName, int> _fieldNameRefCount = new();
+    private readonly ConcurrentDictionary<FieldValue, int> _fieldValueRefCount = new();
+
     public StringField GetStringField(string fieldName, string fieldValue)
     {
         FieldName name = FieldNames.GetOrCreateToken(fieldName);
         FieldValue value = FieldValues.GetOrCreateToken(fieldValue);
-        if (!AllFields.TryGetValue((name,value), out StringField? field))
+
+        if (!AllFields.TryGetValue((name, value), out StringField? field))
         {
-            AllFields[(name,value)] = field = new StringField(name, value);
+            field = new StringField(name, value);
+            AllFields[(name, value)] = field;
+
+            // New field -> increase reference counts
+            _fieldNameRefCount.AddOrUpdate(name, 1, (_, count) => count + 1);
+            _fieldValueRefCount.AddOrUpdate(value, 1, (_, count) => count + 1);
         }
+
         return field;
     }
+
     public StringField InsertStringField(StringField field)
     {
-        if (!AllFields.TryGetValue((field.FieldName,field.Value), out StringField? existingField))
+        if (!AllFields.TryGetValue((field.FieldName, field.Value), out StringField? existingField))
         {
-            AllFields[(field.FieldName,field.Value)] = existingField = new StringField(field.FieldName,field.Value);
+            existingField = new StringField(field.FieldName, field.Value);
+            AllFields[(field.FieldName, field.Value)] = existingField;
+
+            // New field -> increase reference counts
+            _fieldNameRefCount.AddOrUpdate(field.FieldName, 1, (_, count) => count + 1);
+            _fieldValueRefCount.AddOrUpdate(field.Value, 1, (_, count) => count + 1);
         }
+
         return existingField;
     }
-    public List<StringField> InsertStringFields(IEnumerable<StringField> field)
+
+    public List<StringField> InsertStringFields(IEnumerable<StringField> fields)
     {
-        List<StringField> fields = new List<StringField>();
-        foreach (var f in field)
+        List<StringField> insertedFields = new();
+        foreach (var f in fields)
         {
-            fields.Add(InsertStringField(f));
+            insertedFields.Add(InsertStringField(f));
         }
-        return fields;
+        return insertedFields;
     }
 
     public void RemoveStringField(StringField field)
@@ -45,18 +64,17 @@ public class StringFieldPool
         if (!success)
             return;
 
-        // Now check if the FieldName and FieldValue are still used
-        bool fieldNameStillUsed = AllFields.Keys.Any(k => k.Item1 == field.FieldName);
-        bool fieldValueStillUsed = AllFields.Keys.Any(k => k.Item2 == field.Value);
-
-        if (!fieldNameStillUsed)
+        // Decrease reference counts
+        if (_fieldNameRefCount.AddOrUpdate(field.FieldName, 0, (_, count) => count - 1) <= 0)
         {
+            _fieldNameRefCount.TryRemove(field.FieldName, out _);
             FieldNames.RemoveToken(field.FieldName);
         }
-        if (!fieldValueStillUsed)
+
+        if (_fieldValueRefCount.AddOrUpdate(field.Value, 0, (_, count) => count - 1) <= 0)
         {
+            _fieldValueRefCount.TryRemove(field.Value, out _);
             FieldValues.RemoveToken(field.Value);
         }
     }
-
 }
